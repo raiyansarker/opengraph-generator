0ef1870ebc9aeae2744637b212cec610d64a1cd1
Raiyan Sarker <developerraiyan@gmail.com>
2021-12-20 22:29:42 +0600

    Updated svelte config

--- svelte.config.js
+++ svelte.config.js
@@ -1,22 +1,9 @@
-import adapter from '@sveltejs/adapter-auto';
 import cloudflare from '@sveltejs/adapter-cloudflare';
-
-const isProd = process.env.NODE_ENV == 'production';
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
 	kit: {
-		adapter: isProd
-			? cloudflare({
-					target: 'es2020',
-					platform: 'browser',
-					entryPoints: '< input >',
-					outfile: '<output>/_worker.js',
-					allowOverwrite: true,
-					format: 'esm',
-					bundle: true
-			  })
-			: adapter(),
+		adapter: cloudflare(),
 
 		// hydrate the <div id="svelte"> element in src/app.html
 		target: '#svelte'
